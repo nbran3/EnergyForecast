2a6ecdb87dd5a244de5edba8077a3039a451ea5d
nbran3 <nbwan3@gmail.com>
2025-06-06 07:06:47 -0400

    changed weather pipeline to a CSV process

--- prefect/ingest_weather.py
+++ prefect/ingest_weather.py
@@ -21,23 +21,18 @@ def download_weather_data(url, filename):
 
     print(f"File downloaded as {filename}")
 
-
-
 def run_weather_pipeline():
     for i in datasets:
         try:
-            download_weather_data(f"https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/national/time-series/110/{i}/1/0/1973-2025/data.json", f"{i}.json")
-            if os.path.exists(f"{i}.json"):
-                df = pd.read_json(f"{i}.json")
-                df = df.drop('description', axis=1)
-                df = df.drop(['title','missing','units'])
-                df['data'] = df['data'].apply(lambda x: x['value'])
+            download_weather_data(f"https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/national/time-series/110/{i}/1/0/1973-2025/data.csv", f"{i}.csv")
+            if os.path.exists(f"{i}.csv"):
+                df = pd.read_csv(f"{i}.csv", skiprows=3)
                 table_id = f'{dataset_id}.{i}'  
                 print(f'Uploading {i} to BigQuery table {table_id}...')
                 gbq.to_gbq(df, table_id, project_id=project_id, chunksize=10000, if_exists='replace')
                 print(f"Uploaded {i} to BigQuery Successfully")
-                os.remove(f"{i}.json")
+                os.remove(f"{i}.csv")
         except Exception as e:
             print(f"Failed to upload {i} to BigQuery: {e}")
             
-            
+run_weather_pipeline()          
